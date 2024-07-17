a3aaa469b06a02275a21e90be354f14974afb721
Baibhav Mishra <baibhavmishra2010@gmail.com>
2024-07-17 11:18:07 +0530

    updates

--- prometheus_s3/api/upload_new.py
+++ prometheus_s3/api/upload_new.py
@@ -17,9 +17,11 @@ def upload_file(file_path, s3_path, start_dir):
     """
     Function to upload a single file to an S3 bucket
     """
+    print(start_dir, file_path, "hekldsmflkdasjflkdfsjd")
     try:
         # Extract the file name
         file_name = os.path.join(s3_path,os.path.relpath(file_path, start_dir))
+
         # Upload the file
         print(file_path, file_name)
         s3_client.upload_file(file_path, bucket_name, file_name)
@@ -28,18 +30,18 @@ def upload_file(file_path, s3_path, start_dir):
         print(f"Error uploading {file_name}: {e}")
 
 
-# def upload_files_in_parallel(file_paths, s3_path, start_dir):
-#     """
-#     Function to upload files to S3 in parallel using multiprocessing
-#     """
-#     partial_worker = partial(upload_file, s3_path=s3_path, start_dir=start_dir)
-#     with Pool(processes=os.cpu_count()) as pool:
-#         pool.map(partial_worker, file_paths)
+def upload_files_in_parallel(file_paths, s3_path, start_dir):
+    """
+    Function to upload files to S3 in parallel using multiprocessing
+    """
+    partial_worker = partial(upload_file, s3_path=s3_path, start_dir=start_dir)
+    with Pool(processes=os.cpu_count()) as pool:
+        pool.map(partial_worker, file_paths)
 
 
 def get_all_files(directory):
     """
-    Recursively get all files in the given directory
+    Get all files in the given directory
     """
     file_paths = []
     for root, _, files in os.walk(directory):
@@ -48,11 +50,12 @@ def get_all_files(directory):
     return file_paths
 
 
-def upload(name, s3_path):
+def upload(name, s3_path, data):
     s3_client.create_bucket(Bucket=bucket_name)
-    dir = f'/home/baibhav/Downloads/prometheus_new/prometheus-2.52.0.linux-amd64/data/snapshots/{name}'
+    dir = f'{data}/snapshots/{name}'
+    print(dir, 'fuck')
     files_to_upload = get_all_files(dir)
-    upload_file(files_to_upload, s3_path, dir)
+    upload_files_in_parallel(files_to_upload, s3_path, dir)
 
-
-# upload('20240708T085149Z-27b79b8f04946159','localhost:9090/20240708T085149Z-27b79b8f04946159')
+# /home/baibhav/Downloads/prometheus_new/prometheus-2.52.0.linux-amd64/data/
+# upload('20240708T085149Z-27b79b8f04946159','localhost:9090/20240708T085149Z-27b79b8f04946159', '/home/baibhav/Downloads/prometheus_new/prometheus-2.52.0.linux-amd64/data')
--- prometheus_s3/api/views.py
+++ prometheus_s3/api/views.py
@@ -12,16 +12,24 @@ from .download import download
 
 @api_view([GET])
 def save(request):
-    # pass
+    """
+    Use to Save the data in s3 bucket
+    Example: http://127.0.0.1:8000/api/save/?url=localhost:9090&path=/home/baibhav/Downloads/prometheus_new/prometheus-2.52.0.linux-amd64/data/
+    """
     url = request.GET.get("url", None)
     response = requests.post(f"http://{url}/api/v1/admin/tsdb/snapshot")
     path = request.GET.get("path", None)
     data = response.json()
-    upload(data["data"]["name"], f'{url}/{data["data"]["name"]}') 
+    print(path)
+    upload(data["data"]["name"], f'{url}/{data["data"]["name"]}', path) 
     return Response(data)
 
 @api_view([GET])
 def create(request):
+    """
+    Use to download the data from s3 bucket to local system
+    Example: http://127.0.0.1:8000/api/save/?url=localhost:9090&path=/home/baibhav/Downloads/prometheus_new/prometheus-2.52.0.linux-amd64/data/
+    """
     url = request.GET.get("url", None)
     date = request.GET.get("date", None)
     download(url, date)
